495c794f2681ea3cb828db0dd66671614adacc56
Orso <>
2021-10-23 10:54:59 +0200

    .

--- big_no_brainer/model_tf.py
+++ big_no_brainer/model_tf.py
@@ -16,16 +16,6 @@ from big_no_brainer.action import Action
 
 # --------------------------------------------------------------------------------------------------------------------------------
 #   MODEL
-<<<<<<< HEAD
-#--------------------------------------------------------------------------------------------------------------------------------
-# Game() -> Perception() [status(9) mats(10 32 32)] -> model -> Action() [mats(15 32 32)]
-
-def bnb_model_tf(c_in : Perception, c_out : Action):
-    #allocate dictionary of inputs
-
-    c_model = Model( c_in.mats, Sequential( [ Input( shape=(10,32,32), name="mats", dtype=float32 ), layers.Dense(64), layers.Dense(1) ] )  )
-    c_model.compile( loss=losses.MeanSquaredError(), optimizer=optimizers.Adam() )
-=======
 # --------------------------------------------------------------------------------------------------------------------------------
 
 def bnb_model_tf(c_in: Perception, c_out: Action):
@@ -43,6 +33,5 @@ def bnb_model_tf(c_in: Perception, c_out: Action):
     # c_model = Model(c_in.mats, result)
 
     body.compile(loss=losses.MeanSquaredError(), optimizer=optimizers.Adam())
->>>>>>> develop-daniele
 
     return body
