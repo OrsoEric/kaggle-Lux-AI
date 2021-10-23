5451032b08703ae033994603792455abe0a8853a
Orso <>
2021-10-23 10:34:06 +0200

    models compile

--- big_no_brainer/model_tf.py
+++ big_no_brainer/model_tf.py
@@ -16,19 +16,12 @@ from big_no_brainer.action import Action
 #--------------------------------------------------------------------------------------------------------------------------------
 #   MODEL
 #--------------------------------------------------------------------------------------------------------------------------------
+# Game() -> Perception() [status(9) mats(10 32 32)] -> model -> Action() [mats(15 32 32)]
 
 def bnb_model_tf(c_in : Perception, c_out : Action):
     #allocate dictionary of inputs
-    d_input = {}
-    d_input["mats"] = Input(shape=(10,32,32), name="mats", dtype=float32)
-    
 
-    body = Sequential( [ d_input, layers.Dense(64), layers.Dense(1) ] )
-    result = body()
-
-    c_model = Model(d_input, result)
-
-    c_model.compile(loss=losses.MeanSquaredError(),
-                optimizer=optimizers.Adam())
+    c_model = Model( c_in.mats, Sequential( [ Input( shape=(10,32,32), name="mats", dtype=float32 ), layers.Dense(64), layers.Dense(1) ] )  )
+    c_model.compile( loss=losses.MeanSquaredError(), optimizer=optimizers.Adam() )
 
     return c_model
