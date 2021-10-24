595f5dc2332c9690567ae7e36b0e102c7b7f0ae1
jayfranck <franc.jack1@gmail.com>
2021-10-24 12:29:20 +0200

    New model_tf.py with concatenation of inputs (mats and status)

--- big_no_brainer/model_tf.py
+++ big_no_brainer/model_tf.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------------------------------------------------------------------
 #   IMPORT
 # --------------------------------------------------------------------------------------------------------------------------------
-
+import tensorflow as tf
 from tensorflow import float32
 from tensorflow.keras import Input
 from tensorflow.keras import Model
@@ -9,10 +9,11 @@ from tensorflow.keras import Sequential
 from tensorflow.keras import layers
 from tensorflow import losses
 from tensorflow import optimizers
+import pydot
 
 from big_no_brainer.perception import Perception
 from big_no_brainer.action import Action
-
+import Pipeline
 
 # --------------------------------------------------------------------------------------------------------------------------------
 #   MODEL
@@ -20,18 +21,27 @@ from big_no_brainer.action import Action
 
 def bnb_model_tf(c_in: Perception, c_out: Action):
     # allocate dictionary of inputs
-    d_input = {}
-    d_input["mats"] = Input(shape=(10, 32, 32), name="mats", dtype=float32)
+    #d_input = {}
+    #d_input["mats"] = Input(shape=(8, 32, 32), name="mats", dtype=float32)
 
-    # body = Sequential( [ d_input, layers.Dense(64), layers.Dense(1) ] )
-    body = Sequential()
-    body.add(Input(shape=(10, 32, 32), name="mats", dtype=float32))
-    body.add(layers.Dense(64, activation="relu"))
-    body.add(layers.Dense(1, activation="softmax"))
-    # result = body()
+    # # body = Sequential( [ d_input, layers.Dense(64), layers.Dense(1) ] )
+    # body = Sequential()
+    # body.add(Input(shape=(8, 32, 32), name="mats", dtype=float32))
+    # body.add(layers.Dense(64, activation="relu"))
+    # body.add(layers.Dense(1, activation="softmax"))
 
-    # c_model = Model(c_in.mats, result)
+    input_mats = Input(shape=(8, 32, 32), name="mats", dtype=float32)
+    input_mats_flat = layers.Flatten()(input_mats)
+    first_dense = layers.Dense(8192,)(input_mats_flat)
+    input_status = Input(shape=(9,), name="status", dtype=float32)
+    #second_dense = layers.Dense(9,)(input_status)  # dunno if necessary
+    merge = layers.concatenate([first_dense, input_status], axis=1)
+    third_dense = layers.Dense(10240, name="out", dtype=float32)(merge)
+
+    body = Model(inputs=[input_mats, input_status], outputs=third_dense)
 
     body.compile(loss=losses.MeanSquaredError(), optimizer=optimizers.Adam())
+
+    tf.keras.utils.plot_model(model=body,to_file="Model.png", rankdir="LR", dpi=72, show_shapes=True)
 
     return body
